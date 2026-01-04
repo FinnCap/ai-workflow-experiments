1bf82768d1440da1122b118a2e4a6e2ebe683859
FinnCap <finncapelle@yahoo.de>
2026-01-04 16:53:44 +0100

    Fix docker file

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -26,6 +27,12 @@ target_metadata = None
 # ... etc.
 
 
+def get_url():
+    # Get DATABASE_URL from environment, fallback to alembic.ini if not set
+    print(os.getenv("DATABASE_URL"))
+    return os.getenv("DATABASE_URL") or config.get_main_option("sqlalchemy.url")
+
+
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
 
@@ -38,7 +45,8 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = get_url()
+
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -57,16 +65,17 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    configuration = config.get_section(config.config_ini_section)
+    configuration["sqlalchemy.url"] = get_url()
+
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
